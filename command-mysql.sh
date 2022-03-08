1722e809c2f1bde440aed9983d4f4e23a801d764
Bharath Chandra Sudheer <itn.bharath.sudheer@grabtaxi.com>
2022-03-08 15:19:50 +0800

    Remove unused files

--- command-mysql.sh
+++
@@ -1,9 +0,0 @@
-set -e
-set -x
-
-date
-mysql -h 192.168.0.103 -P 3308 -u root -ppassword < mysql.sql
-# date
-# python3 generate-data.py
-date
-mysqlimport  -h 192.168.0.103 -P 3308 -u root -ppassword --local exploration --use-threads=2 --fields-terminated-by=, --verbose transactions.csv
